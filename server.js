0cc1ee1a5f6398bcd36bcb306579626b40e5ed33
Mason <bevinsm355@gmail.com>
2025-06-03 21:01:27 -0400

    Completed everything

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT ?? 3000;
 await db.connect();
 
 app.listen(PORT, () =>{
-    console.log(`Listening onn port ${PORT}...`)
+    console.log(`Listening on port ${PORT}...`)
 })
